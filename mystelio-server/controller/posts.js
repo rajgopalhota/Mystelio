e40e8f5b0c528eb639de59237c26853f7a0bfa9f
rajgopalhota <rajgopalhota@gmail.com>
2023-11-25 23:05:37 +0530

    Posts LOC decreased!

--- mystelio-server/controller/posts.js
+++ mystelio-server/controller/posts.js
@@ -1,12 +1,24 @@
 const Post = require("../models/postModel");
 const User = require("../models/userModel");
 
+// Function to fetch likes with user information
+const fetchLikesWithUserInfo = async (likes) => {
+  return Promise.all(
+    (likes || []).map(async (userId) => {
+      const likedUser = await User.findByPk(userId, {
+        attributes: ["id", "fullName", "email"],
+      });
+      return likedUser;
+    })
+  );
+};
+
+// Create a new post
 exports.addPost = async (req, res) => {
   try {
     const { title, content } = req.body;
-    const userId = req.user.id; // Assuming you have user data in req.user after authentication
+    const userId = req.user.id;
 
-    // Create a new post
     const newPost = await Post.create({
       title,
       content,
@@ -15,95 +27,62 @@ exports.addPost = async (req, res) => {
 
     res.status(201).json({ message: "Post added successfully", post: newPost });
   } catch (error) {
-    console.error("Error registering user:", error);
-    res
-      .status(500)
-      .json({ message: "Error registering user", error: error.message });
+    console.error("Error adding post:", error);
+    res.status(500).json({ message: "Error adding post", error: error.message });
   }
 };
 
+// Fetch posts with user and likes information
+const fetchPostsWithInfo = async (posts) => {
+  return Promise.all(
+    posts.map(async (post) => {
+      const likesWithUserInfo = await fetchLikesWithUserInfo(post.likes);
+      return {
+        id: post.id,
+        title: post.title,
+        content: post.content,
+        createdAt: post.createdAt,
+        likes: likesWithUserInfo,
+        created_user: post.user,
+      };
+    })
+  );
+};
+
+// Fetch posts for the logged-in user
 exports.getLoggedInUserPosts = async (req, res) => {
   try {
     const userId = req.user.id;
-
-    // Find posts for the logged-in user
     const userPosts = await Post.findAll({
       where: { userId: userId },
       attributes: ["id", "title", "content", "createdAt", "likes"],
-      include: [
-        {
-          model: User,
-          as: "user",
-          attributes: ["id", "fullName", "email"],
-        },
-      ],
+      include: [{ model: User, as: "user", attributes: ["id", "fullName", "email"] }],
     });
 
-    const postsWithLikes = await Promise.all(
-      userPosts.map(async (post) => {
-        const likesWithUserInfo = await Promise.all(
-          (post.likes || []).map(async (userId) => {
-            const likedUser = await User.findByPk(userId, {
-              attributes: ["id", "fullName", "email"],
-            });
-            return likedUser;
-          })
-        );
-
-        return {
-          id: post.id,
-          title: post.title,
-          content: post.content,
-          createdAt: post.createdAt,
-          likes: likesWithUserInfo,
-          created_user: post.user,
-        };
-      })
-    );
+    const postsWithLikes = await fetchPostsWithInfo(userPosts);
 
     res.status(200).json({ posts: postsWithLikes });
   } catch (error) {
     console.error("Error fetching user posts:", error);
-    res.status(500).json({
-      message: "Error fetching user posts",
-      error: error.message,
-    });
+    res.status(500).json({ message: "Error fetching user posts", error: error.message });
   }
 };
 
-// Get a single post by ID
+// Fetch a single post by ID
 exports.getPostById = async (req, res) => {
   try {
     const postId = req.params.postId;
-
-    // Find the post by ID
     const post = await Post.findByPk(postId, {
       attributes: ["id", "title", "content", "createdAt", "likes"],
-      include: [
-        {
-          model: User,
-          as: "user",
-          attributes: ["id", "fullName", "email"],
-        },
-      ],
+      include: [{ model: User, as: "user", attributes: ["id", "fullName", "email"] }],
     });
 
-    // Check if the post exists
     if (!post) {
       return res.status(404).json({ message: "Post not found" });
     }
 
-    // Fetch likes with user information
-    const likesWithUserInfo = await Promise.all(
-      (post.likes || []).map(async (userId) => {
-        const likedUser = await User.findByPk(userId, {
-          attributes: ["id", "fullName", "email"],
-        });
-        return likedUser;
-      })
-    );
+    const likesWithUserInfo = await fetchLikesWithUserInfo(post.likes);
 
-    // Construct the response object
     const postWithLikes = {
       id: post.id,
       title: post.title,
@@ -116,57 +95,28 @@ exports.getPostById = async (req, res) => {
     res.status(200).json({ post: postWithLikes });
   } catch (error) {
     console.error("Error fetching post:", error);
-    res
-      .status(500)
-      .json({ message: "Error fetching post", error: error.message });
+    res.status(500).json({ message: "Error fetching post", error: error.message });
   }
 };
 
+// Fetch all posts with user and likes information
 exports.getPosts = async (req, res) => {
   try {
     const allPosts = await Post.findAll({
       attributes: ["id", "title", "content", "createdAt", "likes"],
-      include: [
-        {
-          model: User,
-          as: "user",
-          attributes: ["id", "fullName", "email"],
-        },
-      ],
+      include: [{ model: User, as: "user", attributes: ["id", "fullName", "email"] }],
     });
 
-    const postsWithLikes = await Promise.all(
-      allPosts.map(async (post) => {
-        const likesWithUserInfo = await Promise.all(
-          (post.likes || []).map(async (userId) => {
-            const likedUser = await User.findByPk(userId, {
-              attributes: ["id", "fullName", "email"],
-            });
-            return likedUser;
-          })
-        );
-
-        return {
-          id: post.id,
-          title: post.title,
-          content: post.content,
-          createdAt: post.createdAt,
-          likes: likesWithUserInfo,
-          created_user: post.user,
-        };
-      })
-    );
+    const postsWithLikes = await fetchPostsWithInfo(allPosts);
 
     res.status(200).json({ posts: postsWithLikes });
   } catch (error) {
     console.error("Error fetching posts:", error);
-    res
-      .status(500)
-      .json({ message: "Error fetching posts", error: error.message });
+    res.status(500).json({ message: "Error fetching posts", error: error.message });
   }
 };
 
-// Like and Dislikes
+// Like a post
 exports.likePost = async (req, res) => {
   try {
     const postId = req.params.postId;
@@ -177,35 +127,29 @@ exports.likePost = async (req, res) => {
       return res.status(400).json({ message: "Post not found" });
     }
 
-    // Check if the user has already liked the post
     const alreadyLiked = post.likes.includes(userId);
 
     if (alreadyLiked) {
       return res.status(400).json({ message: "Post already liked" });
     }
 
-    // Add the user ID to the likes array
-    post.likes = [...post.likes, userId]; // Change this line
+    post.likes = [...post.likes, userId];
 
-    // Save the updated post
     try {
       await post.save();
       console.log("Post saved successfully");
       res.status(200).json({ message: "Post liked successfully", post: post });
     } catch (error) {
       console.error("Error saving post:", error);
-      res
-        .status(500)
-        .json({ message: "Error saving post", error: error.message });
+      res.status(500).json({ message: "Error saving post", error: error.message });
     }
   } catch (error) {
     console.error("Error liking post:", error);
-    res
-      .status(500)
-      .json({ message: "Error liking post", error: error.message });
+    res.status(500).json({ message: "Error liking post", error: error.message });
   }
 };
 
+// Unlike a post
 exports.unlikePost = async (req, res) => {
   try {
     const postId = req.params.postId;
@@ -216,34 +160,25 @@ exports.unlikePost = async (req, res) => {
       return res.status(400).json({ message: "Post not found" });
     }
 
-    // Check if the user has already liked the post
     const likedIndex = post.likes.indexOf(userId);
 
     if (likedIndex === -1) {
       return res.status(400).json({ message: "Post not liked yet" });
     }
 
-    // Remove the user ID from the likes array
-    post.likes = post.likes.filter((id) => id !== userId); // Change this line
+    post.likes = post.likes.filter((id) => id !== userId);
 
-    // Save the updated post
     try {
       await post.save();
       console.log("Post saved successfully");
-      res
-        .status(200)
-        .json({ message: "Post unliked successfully", post: post });
+      res.status(200).json({ message: "Post unliked successfully", post: post });
     } catch (error) {
       console.error("Error saving post:", error);
-      res
-        .status(500)
-        .json({ message: "Error saving post", error: error.message });
+      res.status(500).json({ message: "Error saving post", error: error.message });
     }
   } catch (error) {
     console.error("Error unliking post:", error);
-    res
-      .status(500)
-      .json({ message: "Error unliking post", error: error.message });
+    res.status(500).json({ message: "Error unliking post", error: error.message });
   }
 };
 
@@ -253,30 +188,22 @@ exports.deletePost = async (req, res) => {
     const postId = req.params.postId;
     const userId = req.user.id;
 
-    // Find the post
     const post = await Post.findByPk(postId);
 
-    // Check if the post exists
     if (!post) {
       return res.status(400).json({ message: "Post not found" });
     }
 
-    // Check if the authenticated user is the owner of the post
     if (post.userId !== userId) {
-      return res
-        .status(403)
-        .json({ message: "Unauthorized to delete this post" });
+      return res.status(403).json({ message: "Unauthorized to delete this post" });
     }
 
-    // Delete the post
     await post.destroy();
 
     res.status(200).json({ message: "Post deleted successfully" });
   } catch (error) {
     console.error("Error deleting post:", error);
-    res
-      .status(500)
-      .json({ message: "Error deleting post", error: error.message });
+    res.status(500).json({ message: "Error deleting post", error: error.message });
   }
 };
 
@@ -287,32 +214,24 @@ exports.updatePost = async (req, res) => {
     const userId = req.user.id;
     const { title, content } = req.body;
 
-    // Find the post
     const post = await Post.findByPk(postId);
 
     if (!post) {
       return res.status(400).json({ message: "Post not found" });
     }
 
-    // Check if the logged-in user is the creator of the post
     if (post.userId !== userId) {
-      return res
-        .status(403)
-        .json({ message: "You are not authorized to update this post" });
+      return res.status(403).json({ message: "Unauthorized to update this post" });
     }
 
-    // Update the post
     post.title = title || post.title;
     post.content = content || post.content;
 
-    // Save the updated post
     await post.save();
 
     res.status(200).json({ message: "Post updated successfully", post: post });
   } catch (error) {
     console.error("Error updating post:", error);
-    res
-      .status(500)
-      .json({ message: "Error updating post", error: error.message });
+    res.status(500).json({ message: "Error updating post", error: error.message });
   }
 };
